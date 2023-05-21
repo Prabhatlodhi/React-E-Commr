a3135e09f176b227f1449a8af399ed2c8a3b3e88
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-22 04:47:17 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+       <Switch>
+        <Route exact path='/'>
+            <Home/>
+        </Route>
+        <Route exact path='/about'>
+            <About/>
+        </Route>
+        <Route exact path='/cart'>
+            <Cart/>
+        </Route>
+        <Route exact path='/products'>
+            <Products/>
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct/>}>
+            <SingleProduct/>
+        </Route>
+        <Route exact path='/checkout'>
+            <Checkout/>
+        </Route>
+        <Route exact path="*">
+            <Error/>
+        </Route>
+       </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
