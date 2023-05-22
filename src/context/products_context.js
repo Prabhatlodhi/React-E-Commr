e32078b43ae58fabc91fac398b0d469efefc15d4
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-22 18:12:00 +0530

    aa

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -36,7 +36,13 @@ export const ProductsProvider = ({ children }) => {
 
   const fetchProducts = async(url)=>{
     dispatch({type: GET_PRODUCTS_BEGIN})
-    const response = await axios.get(url)
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({type:GET_PRODUCTS_SUCCESS, payload:products})
+    } catch (error) {
+      dispatch({type:GET_PRODUCTS_ERROR})
+    }
   }
 
   useEffect(()=>{
