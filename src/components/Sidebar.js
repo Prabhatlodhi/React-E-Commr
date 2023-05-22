7dc0e2779e1888f92a373318b6f557883e2a1350
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-22 10:17:16 +0530

    aa

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,23 +9,24 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  const isOpen = true
+  const {isSidebarOpen, closeSidebar} = useProductsContext();
+  
   return <SidebarContainer>
-    <aside className={`${isOpen? 'sidebar show-sidebar':'show-sidebar'}`}>
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar':'sidebar'}`}>
       <div className='sidebar-header'>
         <img src={logo} className='logo' alt="comfyslothstore" />
-        <button className='close-btn' type='button'><FaTimes/> </button>
+        <button className='close-btn' type='button' onClick={closeSidebar} ><FaTimes/> </button>
       </div>
       <ul className='links'>
           {links.map(({id, text, url})=>{
             return <li key={id}>
-              <Link to={url}>
+              <Link to={url} onClick={closeSidebar} >
                 {text}
               </Link>
             </li>
           })}
           <li>
-              <Link to='/checkout' >
+              <Link to='/checkout' onClick={closeSidebar}>
                 checkout
               </Link>
             </li>
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ProductsProvider>
+    <ProductsProvider >
         <App />
     </ProductsProvider>
 );
