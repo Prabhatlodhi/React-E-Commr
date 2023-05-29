9ff749063a830083f1049d58febe6f85f9e553fc
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-30 05:03:28 +0530

    aa

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,34 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
+import React from "react";
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
-const Stars = ({stars, reviews}) => {
-  return <Wrapper>
-    <div className="stars">
-      <span>
-        {stars >= 1? (
-        <BsStarFill/> 
-        ) : stars >= 0.5? (
-        <BsStarHalf/> 
-        ) : (
-         <BsStar/>
-         )}
-      </span>
-      <span>
-        {stars >= 2? (
-        <BsStarFill/> 
-        ) : stars >= 1.5? (
-        <BsStarHalf/> 
-        ) : (
-         <BsStar/>
-         )}
-      </span>
-    </div>
-    <p className='reviews'>
-      ({reviews} customer reviews)
-    </p>
-  </Wrapper>
-}
+
+
+const Stars = ({ stars, reviews }) => {  
+  const tempStars = Array.from({length:5},(_,index)=>{
+   const number = index + 0.5
+   return (<span key={index}>
+    {stars >= index+1 ? <BsStarFill /> : stars >= number ? <BsStarHalf /> : <BsStar />}
+    </span>
+)});
+
+return (
+    <Wrapper>
+      <div className="stars">{tempStars} </div>
+      <p className="reviews">({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -43,5 +33,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
