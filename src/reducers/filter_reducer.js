47c2815fc214913cf17c43fd973a2515bcddec6b
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-06-01 03:17:39 +0530

    aa

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -59,6 +59,27 @@ const filter_reducer = (state, action) => {
     const { all_products } = state;
     const { text, category, company, color, price, shipping } = state.filters;
     let tempProducts = [...all_products];
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      });
+    }
+    if (category !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      );
+    }
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color);
+      });
+    }
+
     return { ...state, filtered_products: tempProducts };
   }
 
