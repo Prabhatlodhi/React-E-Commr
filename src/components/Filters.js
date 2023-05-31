7d6295a1315fbd27edfe37bf91b4f10c67d89547
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-31 14:29:39 +0530

    aa

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -11,6 +11,11 @@ const Filters = ({}) => {
     clearFilters,
     all_products,
   } = useFilterContext();
+
+  const categories = getUniqueValues(all_products, "category");
+  const companies = getUniqueValues(all_products, "company");
+  const colors = getUniqueValues(all_products, "colors");
+
   return (
     <Wrapper>
       <div className="content">
@@ -24,6 +29,10 @@ const Filters = ({}) => {
               value={text}
               onChange={updateFilters}
             />
+          </div>
+          <div className="form-control">
+            <h5>category</h5>
+            <div>{}</div>
           </div>
         </form>
       </div>
