4917ffac582f6d45c18c953a7781f8384f2253ff
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-06-01 10:05:41 +0530

    aa

--- src/index.js
+++ src/index.js
@@ -11,12 +11,26 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// dev-wbm4cnlfjyi031sm.us.auth0.com
+//cID
+// xmR94kmfVueZrxnF5l1TQU963zE1sqww
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-wbm4cnlfjyi031sm.us.auth0.com"
+    clientId="xmR94kmfVueZrxnF5l1TQU963zE1sqww"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
